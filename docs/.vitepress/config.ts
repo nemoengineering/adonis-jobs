7efa934c1bd4a83b241ae473b4cc6a86343d28fa
Simone <simone@nemo.ventures>
2024-10-20 15:18:02 +0100

    Add vitepress docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,25 @@
+import {defineConfig} from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Adonis Jobs",
+  description: "Job queues for your AdonisJS applications",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // nav: [
+    //   {text: 'Home', link: '/'},
+    //   {text: 'Examples', link: '/markdown-examples'}
+    // ],
+
+    sidebar: [
+      {text: 'Why Adonis Jobs', link: '/pages/why'},
+      {text: 'Getting Started', link: '/pages/getting-started'},
+      {text: 'Configuration', link: '/pages/configuration'},
+      {text: 'Writing Jobs', link: '/pages/writing-jobs'},
+    ],
+
+    socialLinks: [
+      {icon: 'github', link: 'https://github.com/nemoengineering/adonis-jobs'}
+    ]
+  }
+})
