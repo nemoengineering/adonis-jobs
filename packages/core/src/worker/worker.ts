b242b26aae7003c0204e67b15d9b350fdfb4cbaa
Julien Ripouteau <julien@ripouteau.com>
2025-06-12 00:40:19 +0200

    refactor: refactored worker manager code

---
+++ packages/core/src/worker/worker.ts
@@ -0,0 +1,157 @@
+import { BullMQOtel } from 'bullmq-otel'
+import { trace } from '@opentelemetry/api'
+import { RuntimeException } from '@poppinss/utils'
+import type { Logger } from '@adonisjs/core/logger'
+import type { ApplicationService } from '@adonisjs/core/types'
+import type { EmitterLike } from '@adonisjs/core/types/events'
+
+import { BullMqFactory } from '../bull_factory.js'
+import type { BaseJobConstructor } from '../job/base_job.js'
+import type { BullJob, BullWorker, Config, JobEvents, QueueConfig, Queues } from '../types/index.js'
+
+export class Worker<KnownQueues extends Record<string, QueueConfig> = Queues> {
+  #logger?: Logger
+  #bullWorker?: BullWorker
+  #app: ApplicationService
+  #emitter: EmitterLike<JobEvents>
+  #queueName: keyof KnownQueues
+  #config: Config<KnownQueues>
+  #jobs: Map<string, BaseJobConstructor>
+
+  constructor(options: {
+    app: ApplicationService
+    emitter: EmitterLike<JobEvents>
+    jobs: Map<string, BaseJobConstructor>
+    queueName: keyof KnownQueues
+    config: Config<KnownQueues>
+  }) {
+    this.#app = options.app
+    this.#emitter = options.emitter
+    this.#jobs = options.jobs
+    this.#queueName = options.queueName
+    this.#config = options.config
+  }
+
+  /**
+   * Create a BullMQ worker instance
+   */
+  #createBullWorker(): BullWorker {
+    const queueConfig = this.#config.queues[this.#queueName]
+
+    return BullMqFactory.createWorker(
+      String(this.#queueName),
+      async (job, token) => this.#processJob(job, token),
+      {
+        autorun: false,
+        connection: this.#config.connection,
+        telemetry: new BullMQOtel('adonis-jobs'),
+        ...queueConfig.defaultWorkerOptions,
+        ...(queueConfig?.globalConcurrency && { concurrency: queueConfig.globalConcurrency }),
+      },
+    )
+  }
+
+  async #configureConcurrency(): Promise<void> {
+    if (!this.#bullWorker) return
+
+    const client = await this.#bullWorker.client
+    const globalConcurrency = this.#config.queues[this.#queueName].globalConcurrency
+    if (globalConcurrency) {
+      await client.hset(this.#bullWorker.keys.meta, 'concurrency', globalConcurrency)
+    } else {
+      await client.hdel(this.#bullWorker.keys.meta, 'concurrency')
+    }
+  }
+
+  /**
+   * Start processing a job
+   */
+  async #processJob(job: BullJob, token?: string) {
+    const JobClass = this.#getJobClass(job.name)
+
+    const currentSpan = trace.getActiveSpan()?.setAttribute('bullmq.job.name', job.name)
+    const spanContext = currentSpan?.spanContext()
+    const jobLogger = this.#logger!.child({
+      trace_id: spanContext?.traceId,
+      span_id: spanContext?.spanId,
+      trace_flags: spanContext?.traceFlags?.toString(),
+    })
+
+    const jobInstance = await this.#app.container.make(JobClass)
+    jobInstance.$init(this.#bullWorker!, JobClass, job, token, jobLogger)
+
+    const result = this.#app.container.call(jobInstance, 'process')
+    this.#emitter.emit('job:started', { job })
+
+    return await result
+  }
+
+  /**
+   * Called when a job fails
+   */
+  async #onJobFailed(job: BullJob | undefined, error: Error, token: string) {
+    const spanContext = trace.getActiveSpan()?.spanContext()
+    const logger = this.#logger!.child({
+      trace_id: spanContext?.traceId,
+      span_id: spanContext?.spanId,
+      trace_flags: spanContext?.traceFlags?.toString(),
+    })
+
+    if (!job) return logger.error(error.message, 'Job failed')
+
+    this.#emitter.emit('job:error', { job, error })
+
+    const JobClass = this.#getJobClass(job.name)
+    const jobInstance = await this.#app.container.make(JobClass)
+
+    jobInstance.$init(this.#bullWorker!, JobClass, job, token, this.#logger!)
+    jobInstance.$setError(error)
+
+    await this.#app.container.call(jobInstance, 'onFailed')
+
+    if (jobInstance.allAttemptsMade()) {
+      this.#emitter.emit('job:failed', { job, error })
+    }
+  }
+
+  /**
+   * Called when a job is successfully completed
+   */
+  #onJobCompleted(job: BullJob) {
+    this.#emitter.emit('job:success', { job })
+  }
+
+  #getJobClass(jobName: string) {
+    const JobClass = this.#jobs.get(jobName)
+    if (JobClass) return JobClass
+
+    const message = `Job "${String(jobName)}" was not found. If you changed the job class name, make sure to use update the 'nameOverride' property.`
+    throw new RuntimeException(message)
+  }
+
+  /**
+   * Start the worker
+   */
+  async start(): Promise<BullWorker> {
+    if (this.#bullWorker) throw new RuntimeException('Worker is already started')
+
+    const loggerService = await this.#app.container.make('logger')
+    this.#logger = loggerService.child({ queueName: this.#queueName })
+
+    this.#bullWorker = this.#createBullWorker()
+    await this.#configureConcurrency()
+
+    this.#bullWorker.on('failed', this.#onJobFailed.bind(this))
+    this.#bullWorker.on('completed', this.#onJobCompleted.bind(this))
+
+    this.#bullWorker.run()
+    return this.#bullWorker
+  }
+
+  async stop(force = false): Promise<void> {
+    if (!this.#bullWorker) return
+
+    await this.#bullWorker.close(force)
+    this.#bullWorker = undefined
+  }
+}
--- packages/core/src/worker/worker_manager.ts
+++ packages/core/src/worker/worker_manager.ts
@@ -1,20 +1,15 @@
-import { BullMQOtel } from 'bullmq-otel'
-import { trace } from '@opentelemetry/api'
-import { RuntimeException } from '@poppinss/utils'
 import type { ApplicationService } from '@adonisjs/core/types'
 import type { EmitterLike } from '@adonisjs/core/types/events'
 
-import { BullMqFactory } from '../bull_factory.js'
-import { JobDiscoverer } from './job_discoverer.js'
+import { Worker } from './worker.js'
 import type { BaseJobConstructor } from '../job/base_job.js'
-import type { BullWorker, Config, JobEvents, QueueConfig, Queues } from '../types/index.js'
+import type { Config, JobEvents, QueueConfig, Queues } from '../types/index.js'
 
 export class WorkerManager<KnownQueues extends Record<string, QueueConfig> = Queues> {
   readonly #app: ApplicationService
   readonly #emitter: EmitterLike<JobEvents>
-
   readonly #jobs: Map<string, BaseJobConstructor>
-  #runningWorkers: BullWorker[] = []
+  #runningWorkers: Worker<KnownQueues>[] = []
 
   constructor(
     app: ApplicationService,
@@ -31,117 +26,27 @@ export class WorkerManager<KnownQueues extends Record<string, QueueConfig> = Que
     return Object.keys(this.config.queues) as (keyof KnownQueues)[]
   }
 
-  async startWorkers(queueNames: (keyof KnownQueues)[]) {
-    this.#runningWorkers = await Promise.all(queueNames.map((w) => this.#startWorker(w)))
-  }
-
-  async stopWorkers() {
-    await Promise.all(this.#runningWorkers.map((w) => w.close()))
-  }
-
-  async #startWorker(queueName: keyof KnownQueues) {
-    const loggerService = await this.#app.container.make('logger')
-    const logger = loggerService.child({ queueName })
-
-    const worker = BullMqFactory.createWorker(
-      String(queueName),
-      async (job, token) => {
-        const currentSpan = trace.getActiveSpan()
-        currentSpan?.setAttribute('bullmq.job.name', job.name)
-        const spanContext = currentSpan?.spanContext()
-
-        const JobClass = this.#getJobClass(job.name)
-
-        const jobInstance = await this.#app.container.make(JobClass)
-        jobInstance.$init(
-          worker,
-          JobClass,
-          job,
-          token,
-          logger.child({
-            trace_id: spanContext?.traceId,
-            span_id: spanContext?.spanId,
-            trace_flags: spanContext?.traceFlags?.toString(),
-          }),
-        )
-
-        jobInstance.logger.info('Starting job')
-
-        const res = this.#app.container.call(jobInstance, 'process')
-        void this.#emitter.emit('job:started', { job })
-        return await res
-      },
-      {
-        autorun: false,
-        connection: this.config.connection,
-        ...(this.config.queues[queueName]?.globalConcurrency && {
-          concurrency: this.config.queues[queueName].globalConcurrency,
-        }),
-        ...this.config.queues[queueName].defaultWorkerOptions,
-        telemetry: new BullMQOtel('adonis-jobs'),
-      },
-    )
-
-    const client = await worker.client
-    if (this.config.queues[queueName].globalConcurrency) {
-      await client.hset(
-        worker.keys.meta,
-        'concurrency',
-        this.config.queues[queueName].globalConcurrency,
-      )
-    } else {
-      await client.hdel(worker.keys.meta, 'concurrency')
-    }
-
-    worker.on('failed', async (job, error, token) => {
-      const spanContext = trace.getActiveSpan()?.spanContext()
-      const errLogger = logger.child({
-        trace_id: spanContext?.traceId,
-        span_id: spanContext?.spanId,
-        trace_flags: spanContext?.traceFlags?.toString(),
-      })
-
-      if (!job) {
-        errLogger.error(error.message, 'Job failed')
-        return
-      }
-      try {
-        void this.#emitter.emit('job:error', { job, error })
-
-        const JobClass = this.#getJobClass(job.name)
-        const jobInstance = await this.#app.container.make(JobClass)
-        jobInstance.$init(worker, JobClass, job, token, logger)
-        jobInstance.$setError(error)
-
-        await this.#app.container.call(jobInstance, 'onFailed')
-
-        if (jobInstance.allAttemptsMade()) {
-          void this.#emitter.emit('job:failed', { job, error })
-        }
-      } catch (e) {
-        errLogger.error(e)
-      }
+  async #startWorker(queueName: keyof KnownQueues): Promise<Worker<KnownQueues>> {
+    const worker = new Worker({
+      queueName,
+      app: this.#app,
+      jobs: this.#jobs,
+      config: this.config,
+      emitter: this.#emitter,
     })
 
-    worker.on('completed', (job) => {
-      this.#emitter.emit('job:success', { job })
-    })
-
-    void worker.run()
+    worker.start()
     return worker
   }
 
-  #getJobClass(jobName: string) {
-    const JobClass = this.#jobs.get(jobName)
-    if (!JobClass)
-      throw new RuntimeException(
-        `Unknown job "${String(jobName)}". Make sure it is configured inside the config file`,
-      )
-    return JobClass
+  async startWorkers(queueNames: (keyof KnownQueues)[]) {
+    this.#runningWorkers = await Promise.all(
+      queueNames.map((queueName) => this.#startWorker(queueName)),
+    )
   }
 
-  static async loadJobs(app: ApplicationService): Promise<BaseJobConstructor[]> {
-    const discoverer = new JobDiscoverer(app.appRoot)
-    return await discoverer.discoverJobs()
+  async stopWorkers(force = false) {
+    for (const worker of this.#runningWorkers) await worker.stop(force)
+    this.#runningWorkers = []
   }
 }
